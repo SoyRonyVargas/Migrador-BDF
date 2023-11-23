2af4f0986131fba6460c3c386238946118eb371f
Carlos Catalan <carloscatalan@gmail.com>
2023-11-23 01:03:57 -0500

    cambios archivo

--- migracion-materia.py
+++ migracion-materia.py
@@ -30,6 +30,111 @@ source_cursor = source_conn.cursor()
 destination_conn = pyodbc.connect(destination_connection_string)
 destination_cursor = destination_conn.cursor()
 
+def insertarUnidadesTematicas( idAsignatura , objetivos ):
+    
+    insert_query = "INSERT INTO UnidadesTematicas (Nombre, Objetivos, HorasPracticas, HorasTeoricas, Version, Eliminado , IdAsignatura) VALUES (?, ?, ?, ?, ?, ?, ?)"
+
+    for i in objetivos:
+        
+        current_datetime = datetime.now()
+
+        insercion = {
+            'Nombre': i,
+            'Objetivos': "",
+            'HorasPracticas': 0,
+            'HorasTeoricas':0,
+            'Version': current_datetime,
+            'Eliminado': 0,
+            'IdAsignatura': idAsignatura,
+            'Version': current_datetime, 
+            'Eliminado': 0,
+        }
+        
+        values_tuple = tuple(insercion.values())
+    
+        destination_cursor.execute(insert_query, values_tuple)
+    
+    destination_conn.commit()
+    
+    return
+
+def insertarPlanEstudioRelacion( idAsignatura , planes ):
+    
+    insert_query = "INSERT INTO PlanesEstudios (Nombre, TipoPlanEstudio, Version, Eliminado) VALUES (?, ?, ?, ?)"
+    
+    insert_query_relacion = "INSERT INTO PlanesEstudioAsignaturas (IdAsignatura, IdPlanEstudio, Version, Eliminado) VALUES (?, ?, ?, ?)"
+
+    for i in planes:
+        
+        current_datetime = datetime.now()
+
+        insercion = {
+            'Nombre': i,
+            'TipoPlanEstudio': 0,
+            'Version': current_datetime,
+            'Eliminado': 0,
+        }
+        
+        values_tuple = tuple(insercion.values())
+    
+        destination_cursor.execute(insert_query, values_tuple)
+        
+        destination_conn.commit()
+
+        inserted_id = destination_cursor.execute("SELECT @@IDENTITY").fetchone()[0]
+
+        insercionRelacion = {
+            'IdAsignatura': idAsignatura,
+            'IdPlanEstudio': inserted_id,
+            'Version': current_datetime,
+            'Eliminado': 0,
+        }
+        
+        values_tuple = tuple(insercionRelacion.values())
+
+        destination_cursor.execute(insert_query_relacion, values_tuple)
+        
+        destination_conn.commit()
+
+    return
+
+
+# try:
+
+#     select_query = "SELECT * FROM DPERSO"
+    
+#     source_cursor.execute(select_query)
+    
+#     data_to_move = source_cursor.fetchall()
+    
+#     for row in data_to_move:
+        
+#         insert_query = "INSERT INTO Personas (Nombre, Apellidos, CURP,Discriminator, Area) VALUES (?, ?, ?, ?, ?)"
+        
+#         current_datetime = datetime.now()
+
+#         insercion = {
+#             'Nombre': row[3],
+#             'Apellidos': f"{row[1]} {row[2]}",
+#             'CURP': row[7],
+#             # 'IdApplicationUser': "SP1",
+#             'Discriminator': "",
+#             'Area': '',
+#         }
+
+#         # Convertir el diccionario a una tupla de pares clave-valor
+#         values_tuple = tuple(insercion.values())
+
+#         # Ejecutar la consulta de inserción
+#         destination_cursor.execute(insert_query, values_tuple)
+
+#     destination_conn.commit()
+
+#     print("Terminado Persona")
+
+# finally:
+#     print()
+
 try:
     # Leer datos de la primera tabla (DMATER)
     select_query = "SELECT * FROM DMATER"
@@ -38,10 +143,13 @@ try:
 
     # Insertar datos en la segunda tabla (Asignaturas)
     insert_query = "INSERT INTO Asignaturas (NombrePE, Nombre, NombreTitulo, Objetivos, Competencias, HorasPracticas, HorasTeoricas, Version, Eliminado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    
+    index = 0
 
     for row in data_to_move:
         
-        print(row)
+        print(f"Insertando {index}")
+        # print(row)
 
         current_datetime = datetime.now()
 
@@ -57,13 +165,58 @@ try:
             'Eliminado': 0,
         }
 
-        print(insercion)
+        # print(row[12])
+        # print(row[13])
+        # print(row[14])
+        # print(row[15])
+        # print(row[16])
+
+        # print(insercion)
 
         # Convertir el diccionario a una tupla de pares clave-valor
         values_tuple = tuple(insercion.values())
 
         # Ejecutar la consulta de inserción
         destination_cursor.execute(insert_query, values_tuple)
+        
+        # obtener el ultimo id
+        inserted_id = destination_cursor.execute("SELECT @@IDENTITY").fetchone()[0]
+
+        unidades = list(filter(lambda x: "None" != x , [ row[12] , row[13] , row[14] , row[15] , row[16] ]))
+
+        print(inserted_id)
+        # print(unidades)
+
+        insertarUnidadesTematicas( inserted_id , unidades )
+        
+        # print(row[57:80])
+        planes = list(filter(lambda x: "None" != x , [ 
+            row[57], 
+            row[58], 
+            row[59], 
+            row[60], 
+            row[61],
+            row[62],
+            row[63],
+            row[64],
+            row[65],
+            row[66],
+            row[67],
+            row[68],
+            row[69],
+            row[70]
+        ]))
+
+        print(planes)
+        
+        # raise ValueError("Esto es un mensaje de error")
+
+        #57
+        insertarPlanEstudioRelacion( inserted_id , planes )
+        
+        # insertarPlanEstudioRelacion( inserted_id , unidades )
+
+        index+=1
 
     # Confirmar los cambios en la base de datos de destino
     destination_conn.commit()
